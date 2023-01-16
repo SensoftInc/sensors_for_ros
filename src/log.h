db99bfa5d7634c5da5ccc26cf484d9646e93cf37
mconte <XM97WA@spxcorp.com>
2023-01-16 08:49:58 -0500

    Remove unnecessary folders

--- src/log.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <android/log.h>
-
-#define LOGI(...) \
-  ((void)__android_log_print(ANDROID_LOG_INFO, "sensors_for_ros", __VA_ARGS__))
-#define LOGW(...) \
-  ((void)__android_log_print(ANDROID_LOG_WARN, "sensors_for_ros", __VA_ARGS__))
